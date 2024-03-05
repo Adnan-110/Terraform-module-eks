73b696f6d89f1b1a5f7638bccf584b93f1d908b4
Adnan <adnanshaikh11501@gmail.com>
2024-03-05 14:03:38 +0530

    k8-deploy.yml create/destroy pipeline

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "eks" {
   name     = "${var.ENV}-eks-cluster"
   role_arn = aws_iam_role.eks-cluster-role.arn
-  version  = "1.25"
+  version  = "1.24"
 
   vpc_config {
     subnet_ids = var.PRIVATE_SUBNET_IDS
